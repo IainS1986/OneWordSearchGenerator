020387c96f0de4299796175320a935323ca5a4ae
Iain <iain.stanford@moasure.com>
2020-11-29 22:19:31 +0000

    Python - Changed from 2D to 1D array
    Should be faster.

--- OneWordSearchGenerator/OneWordSearchGenerator.py
+++ OneWordSearchGenerator/OneWordSearchGenerator.py
@@ -21,18 +21,18 @@ def scramble(word):
     shuffle(word)
     return ''.join(word)
 
-def printGrid(grid):
-    for y in grid:
-        output = ''
-        for x in y:
-            output = output + x
-        print output
+def printGrid(grid, size):
+    for y in range(size):
+        output = list()
+        for x in range(size):
+            output.append(grid[(y * size) + x])
+        print ''.join(output)
 
-def insertWord(grid, word):
+def insertWord(grid, word, size):
     minx = 2
-    maxx = len(grid) - 3
+    maxx = size - 3
     miny = 2
-    maxy = len(grid) - 3
+    maxy = size - 3
     vx = 0
     vy = 0
     d = Direction(random.randint(0, 7))
@@ -54,37 +54,39 @@ def insertWord(grid, word):
     y = random.randint(miny, maxy)
 
     for c in word:
-        grid[y][x] = c
+        grid[(y * size) + x] = c
         x += vx
         y += vy
 
     print 'Direction : {0}'.format(d)
-    return x, y
+    return (x - vx), (y - vy)
 
-def pointInGrid(grid, x, y):
-    if x >= 0 and x < len(grid) and y >= 0 and y < len(grid):
+def pointInGrid(x, y, size):
+    if x >= 0 and x < size and y >= 0 and y < size:
         return True
     return False
 
-def CheckWordNotThere(grid, x, y, c, word):
+def CheckWordNotThere(grid, x, y, c, word, size):
+    reverse = word[::-1]
     span = len(word) - 1
     letters = list()
 
     # First Check Up and Down
     # Add the letters above
     for i in xrange(span, 0, -1):
-        if (pointInGrid(grid, x, y - i)):
-            letters.append(grid[y - i][x])
+        if (pointInGrid(x, y - i, size)):
+            letters.append(grid[((y - i) * size) + x])
 
     # Add the letter
     letters.append(c)
     # Add the letters after
     for i in xrange(1, span):
-        if (pointInGrid(grid, x, y + i)):
-            letters.append(grid[y + i][x])
+        if (pointInGrid(x, y + i, size)):
+            letters.append(grid[((y + i) * size) + x])
 
     # Check result
-    if word in ''.join(letters):
+    result = ''.join(letters)
+    if word in result or reverse in result:
         return False
 
 
@@ -93,18 +95,19 @@ def CheckWordNotThere(grid, x, y, c, word):
     # Second check left and right
     del letters [:]
     for i in xrange(span, 0, -1):
-        if (pointInGrid(grid, x - i, y)):
-            letters.append(grid[y][x - i])
+        if (pointInGrid(x - i, y, size)):
+            letters.append(grid[(y * size) + x - i])
 
     # Add the letter
     letters.append(c)
     # Add the letters after
     for i in xrange(1, span):
-        if (pointInGrid(grid, x + i, y)):
-            letters.append(grid[y][x + i])
+        if (pointInGrid(x + i, y, size)):
+            letters.append(grid[(y * size) + x + i])
 
     # Check result
-    if word in ''.join(letters):
+    result = ''.join(letters)
+    if word in result or reverse in result:
         return False
 
 
@@ -113,18 +116,19 @@ def CheckWordNotThere(grid, x, y, c, word):
     # Thirdly check DownLeft and UpRight
     del letters [:]
     for i in xrange(span, 0, -1):
-        if (pointInGrid(grid, x - i, y + i)):
-            letters.append(grid[y + i][x - i])
+        if (pointInGrid(x - i, y + i, size)):
+            letters.append(grid[((y + i) * size) + x - i])
 
     # Add the letter
     letters.append(c)
     # Add the letters after
     for i in xrange(1, span):
-        if (pointInGrid(grid, x + i, y - i)):
-            letters.append(grid[y - i][x + i])
+        if (pointInGrid(x + i, y - i, size)):
+            letters.append(grid[((y - i) * size) + x + i])
 
     # Check result
-    if word in ''.join(letters):
+    result = ''.join(letters)
+    if word in result or reverse in result:
         return False
 
 
@@ -133,57 +137,85 @@ def CheckWordNotThere(grid, x, y, c, word):
     # Finally check UpLeft and DownRight
     del letters [:]
     for i in xrange(span, 0, -1):
-        if (pointInGrid(grid, x - i, y - i)):
-            letters.append(grid[y - i][x - i])
+        if (pointInGrid(x - i, y - i, size)):
+            letters.append(grid[((y - i) * size) + x - i])
 
     # Add the letter
     letters.append(c)
     # Add the letters after
     for i in xrange(1, span):
-        if (pointInGrid(grid, x + i, y + i)):
-            letters.append(grid[y + i][x + i])
+        if (pointInGrid(x + i, y + i, size)):
+            letters.append(grid[((y + i) * size) + x + i])
 
     # Check result
-    if word in ''.join(letters):
+    result = ''.join(letters)
+    if word in result or reverse in result:
         return False
 
     # No word present!
     return True
 
-def insertLetter(grid, x, y, word, extra):
-    concat = word
-    if extra is not '':
-        concat += extra
-    concat = scramble(concat)
+def insertLetter(grid, x, y, word, extra, size):
+    # Build the list of chars to insert in order we want to try
+    # First, add all the neighbouring letters
+    # Then add the remaining letters (scrambled) in front, this means
+    # neighbouring letters should be attempted last, so result in less
+    # blocks of the same letters
+    neighbours = list()
+    if (pointInGrid(x - 1, y, size)):
+        c = grid[(y * size) + x - 1]
+        if (c is not '.' and c not in neighbours):
+            neighbours.append(c)
+    if (pointInGrid(x + 1, y, size)):
+        c = grid[(y * size) + x + 1]
+        if (c is not '.' and c not in neighbours):
+            neighbours.append(c)
+    if (pointInGrid(x, y - 1, size)):
+        c = grid[((y - 1) * size) + x]
+        if (c is not '.' and c not in neighbours):
+            neighbours.append(c)
+    if (pointInGrid(x, y + 1, size)):
+        c = grid[((y + 1) * size) + x]
+        if (c is not '.' and c not in neighbours):
+            neighbours.append(c)
 
-    r = random.randint(0, len(concat) - 1)
+    # merge the word and extra work together, and scramble letters
+    # so that we don't try to insert letters in the same order every time.
+    letters = list()
+    for i in range(len(word)):
+        if word[i] not in neighbours:
+            letters.append(word[i])
+    for i in range(len(extra)):
+        if extra[i] not in neighbours:
+            letters.append(extra[i])
+
+    # Shuffle letters
+    random.shuffle(letters)
+
+    # Add neighbours onto end
+    for i in range(len(neighbours)):
+        letters.append(neighbours[i])
 
     # Loop through word trying to insert a letter such that it does NOT make the word in any direction
-    for i in xrange(len(concat) - 1):
-        ci = i + r
-        if ci >= len(concat):
-            ci -= len(concat)
-        
-        c = concat[ci]
+    for i in xrange(len(letters) - 1):
+        c = letters[i]
 
-        canInsert = CheckWordNotThere(grid, x, y, c, word)
+        canInsert = CheckWordNotThere(grid, x, y, c, word, size)
         if not canInsert:
             continue
 
         if extra is not '':
-            canInsert = CheckWordNotThere(grid, x, y, c, extra)
+            canInsert = CheckWordNotThere(grid, x, y, c, extra, size)
             if not canInsert:
                 continue
         
         if canInsert:
-            grid[y][x] = c
+            grid[(y * size) + x] = c
             return True
     
     return False
 
-def fillGrid(grid, end, word, extra):
-    maxx = len(grid)
-    maxy = len(grid)
+def fillGrid(grid, end, word, extra, size):
     q = queue.Queue()
     q.put(end)
     while not q.empty():
@@ -193,23 +225,23 @@ def fillGrid(grid, end, word, extra):
         y = p[1]
 
         # Check if grid value is blank
-        if grid[y][x] is '.':
-            success = insertLetter(grid, x, y, word, extra)
+        if grid[(y * size) + x] is '.':
+            success = insertLetter(grid, x, y, word, extra, size)
             if not success:
                 return False
             
         
         # Queue up blank spaces around current point
-        if (y > 0 and grid[y - 1][x] is '.'):
+        if (y > 0 and grid[((y - 1) * size) + x] is '.'):
             q.put((x, y - 1))
 
-        if (y < maxy - 1 and grid[y + 1][x] is '.'):
+        if (y < size - 1 and grid[((y + 1) * size) + x] is '.'):
             q.put((x, y + 1))
 
-        if (x > 0 and grid[y][x - 1] is '.'):
+        if (x > 0 and grid[(y * size) + x - 1] is '.'):
             q.put((x - 1, y))
 
-        if (x < maxx - 1 and grid[y][x + 1] is '.'):
+        if (x < size - 1 and grid[(y * size) + x + 1] is '.'):
             q.put((x + 1, y))
     
     return True
@@ -230,20 +262,20 @@ def main():
         random.seed = args.seed
 
     # Initialize the grid
-    grid = [['.' for x in xrange(args.size)] for y in xrange(args.size)]
+    grid = ['.' for i in xrange(args.size * args.size)]
 
     # Insert word
-    end = insertWord(grid, args.word)
+    end = insertWord(grid, args.word, args.size)
     print end
-    printGrid(grid)
+    printGrid(grid, args.size)
 
     # Fill Grid with letters
-    success = fillGrid(grid, end, args.word, args.extra)
+    success = fillGrid(grid, end, args.word, args.extra, args.size)
     if not success:
         print '--- FAILED ---'
     else:
         print 'Success!'
-    printGrid(grid)
+    printGrid(grid, args.size)
 
 if __name__ == '__main__':
     main()
